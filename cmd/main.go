e044d7d78a38fe8245ee458b39db9e878ed98031
Gabriel Freitas <gabriielfs96@gmail.com>
2024-11-21 17:32:23 -0300

    feat: added mioddlewares

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+
+	"log/slog"
 
 	"github.com/FreitasGabriel/fullcycle-api/configs"
 	"github.com/FreitasGabriel/fullcycle-api/internal/entity"
@@ -9,21 +13,28 @@ import (
 	"github.com/FreitasGabriel/fullcycle-api/internal/infra/webserver/handler"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := configs.LoadConfig(".")
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("Stargin application")
+
+	logger.Info("Loading config")
+	config, err := configs.LoadConfig(".")
 	if err != nil {
 		panic(err)
 	}
 
+	logger.Info("Connecting to database")
 	db, err := gorm.Open(sqlite.Open("teste.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
+	logger.Info("Running migrations")
 	err = db.AutoMigrate(&entity.User{}, &entity.Product{})
 	if err != nil {
 		panic(err)
@@ -34,16 +45,35 @@ func main() {
 	productHandler := handler.NewProductHandler(productDB)
 	userHandler := handler.NewUserHandler(userDB)
 
+	logger.Info("Starting server")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Get("/products", productHandler.GetAllProducts)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	r.Use(middleware.Recoverer)
+	// r.Use(LogRequest)
+	r.Use(middleware.WithValue("jwt", config.TokenAuthKey))
+	r.Use(middleware.WithValue("jwtExpiresIn", config.JWTExpiresIn))
 
-	r.Post("/user", userHandler.CreateUser)
-	// r.Get("/users", userHandler.GetAllUsers)
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(config.TokenAuthKey))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetAllProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Route(("/user"), func(r chi.Router) {
+		r.Post("/", userHandler.CreateUser)
+		r.Post("/generate_token", userHandler.GetJWT)
+	})
 
 	http.ListenAndServe(":8000", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
--- internal/infra/webserver/handler/user_handler.go
+++ internal/infra/webserver/handler/user_handler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/FreitasGabriel/fullcycle-api/internal/dto"
 	"github.com/FreitasGabriel/fullcycle-api/internal/entity"
@@ -12,7 +14,6 @@ import (
 
 type UserHandler struct {
 	UserDB       database.UserInterface
-	JWT          *jwtauth.JWTAuth
 	JWTExpiresIn int
 }
 
@@ -20,6 +21,50 @@ func NewUserHandler(userDB database.UserInterface) *UserHandler {
 	return &UserHandler{
 		UserDB: userDB,
 	}
+}
+
+func (uh *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
+	jwtExpiresIn := r.Context().Value("jwtExpiresIn").(int)
+
+	var user dto.LoginInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	u, err := uh.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	_, tokenString, err := jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
+	})
+	if err != nil {
+		fmt.Println("error to generate token", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	accessToken := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: tokenString,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(accessToken)
+	w.WriteHeader(http.StatusOK)
+
 }
 
 func (uh *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
